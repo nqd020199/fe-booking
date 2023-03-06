8ec6723f934ec5d9d79fca0c15528c9cca18a4a6
dai nguyen <dainguyen020199@gmail.com>
2023-03-06 11:25:28 +0700

    #eric update

--- src/pages/Authencation/index.tsx
+++ src/pages/Authencation/index.tsx
@@ -2,21 +2,14 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { OptionsChild } from '../../components/molecules/Dropdown/index';
-
 import Typography from 'components/atoms/Typography';
-import Dropdown from 'components/molecules/Dropdown';
 import Container from 'components/organisms/Container';
-import { optionsLanguage } from 'components/templates/MainLayout';
 import RenderForm from 'components/templates/RenderForm';
-import i18n from 'i18n';
-import { increment } from 'store/example';
-import { useAppDispatch, useAppSelector } from 'store/hooks';
+import { useAppSelector } from 'store/hooks';
 import { USER_LOGIN } from 'utils/constants';
 
 const Authencation: React.FC = () => {
   const navigator = useNavigate();
-  const dispatch = useAppDispatch();
   const language = useAppSelector((state) => state.example.language);
 
   useEffect(() => {
@@ -24,12 +17,6 @@ const Authencation: React.FC = () => {
       navigator('/');
     }
   }, []);
-
-  const handleChangeLang = (option: OptionsChild) => {
-    i18n.changeLanguage(option.acronym);
-    dispatch(increment(option));
-    sessionStorage.setItem('currLanguage', JSON.stringify(option));
-  };
 
   useEffect(() => {
     if (language.acronym !== 'KO') {
@@ -42,15 +29,6 @@ const Authencation: React.FC = () => {
   return (
     <div className="p-anthencation">
       <Container modifiers={['form']}>
-        <div className="p-anthencation_language">
-          <Dropdown
-            optionsChild={optionsLanguage || []}
-            handleClick={(item) => handleChangeLang(item)}
-            typeDropdown="dropDown"
-            iconName={language.IconName}
-            name={language.acronym}
-          />
-        </div>
         <div className="p-anthencation_left">
           <Typography content="Booking<br />Management" modifiers={['blueNavy', 'capitalize', '46x50', '700']} />
           <div className="p-anthencation_left-mask">
@@ -62,12 +40,6 @@ const Authencation: React.FC = () => {
           <RenderForm />
         </div>
       </Container>
-      <span className="p-anthencation_neumorphism size-sm" />
-      <span className="p-anthencation_neumorphism size-md" />
-      <span className="p-anthencation_neumorphism size-lg" />
-      <span className="p-anthencation_circle" />
-      <span className="p-anthencation_circle-dou1" />
-      <span className="p-anthencation_circle-dou2" />
     </div>
   );
 };
